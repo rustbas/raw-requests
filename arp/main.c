04be4dd83f2ad335b3cebecd64f12467fa0d2c40
rustam <rustam@honor-debian.saccada.local>
2026-06-23 02:38:21 +0300

    autopretty by clangd
    do not want to fix it

--- arp/main.c
+++ arp/main.c
@@ -2,48 +2,47 @@
 #include <net/if.h>
 #include <netdb.h>
 #include <netinet/in.h>
+#include <netpacket/packet.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <sys/ioctl.h>
 #include <sys/socket.h>
-#include <netpacket/packet.h>
 
-#define PROTOCOL_TYPE_SHIFT  22
-#define REQUEST_TYPE_SHIFT   12
-#define PORT_SHIFT           2
-#define IF_NAME              "wlp0s20f3"
-#define MAC_LEN              6
-#define IP_LEN               4
+#define PROTOCOL_TYPE_SHIFT 22
+#define REQUEST_TYPE_SHIFT 12
+#define PORT_SHIFT 2
+#define IF_NAME "wlp1s0"
+#define MAC_LEN 6
+#define IP_LEN 4
 
-#define PRINT_DELIM \
+#define PRINT_DELIM                                                            \
   "=========================================================\n"
 
-typedef unsigned char  u8;
+typedef unsigned char u8;
 typedef unsigned short u16;
 
 typedef struct {
-  u8  dst[MAC_LEN];
-  u8  src[MAC_LEN];
+  u8 dst[MAC_LEN];
+  u8 src[MAC_LEN];
   u16 type;
 } eth_header;
 
 typedef struct {
   u16 hw_type;
   u16 proto_type;
-  u8  hw_size;
-  u8  proto_size;
+  u8 hw_size;
+  u8 proto_size;
   u16 opcode;
-  u8  sender_mac[MAC_LEN];
-  u8  sender_ip[IP_LEN];
-  u8  target_mac[MAC_LEN];
-  u8  target_ip[IP_LEN];
+  u8 sender_mac[MAC_LEN];
+  u8 sender_ip[IP_LEN];
+  u8 target_mac[MAC_LEN];
+  u8 target_ip[IP_LEN];
 } arp_header;
 
-int main(){
+int main() {
   struct sockaddr_ll sll = {0};
   struct ifreq ifr;
-  
+
   int fd, if_index, i;
   eth_header eth;
   arp_header arp;
@@ -52,7 +51,7 @@ int main(){
   ssize_t ret;
 
   /* Ethernet header fill */
-  eth.type = htons(0x0806);  
+  eth.type = htons(0x0806);
   memset(eth.dst, 0xFF, MAC_LEN); /* Broadcasing MAC-address */
 
   /* ARP header fill */
@@ -63,16 +62,16 @@ int main(){
   arp.opcode = htons(0x0001);
   memset(arp.target_mac, 0x00, MAC_LEN);
   /* Router IP */
-  arp.target_ip[0] = 192;
-  arp.target_ip[1] = 168;
-  arp.target_ip[2] = 2;
+  arp.target_ip[0] = 127;
+  arp.target_ip[1] = 0;
+  arp.target_ip[2] = 0;
   arp.target_ip[3] = 1;
 
   printf("Target IP:   ");
-  for (i=0; i<IP_LEN; i++)
-    printf("%d%c", arp.target_ip[i], i==3?'\n':'.');    
+  for (i = 0; i < IP_LEN; i++)
+    printf("%d%c", arp.target_ip[i], i == 3 ? '\n' : '.');
   printf(PRINT_DELIM);
-  
+
   /* Get socket descriptor */
   fd = socket(PF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
   if (fd == -1)
@@ -86,66 +85,63 @@ int main(){
   if_index = ifr.ifr_ifindex;
   if (ioctl(fd, SIOCGIFHWADDR, &ifr) < 0)
     return 1;
-  
-  memcpy(eth.src,        ifr.ifr_hwaddr.sa_data, MAC_LEN);
+
+  memcpy(eth.src, ifr.ifr_hwaddr.sa_data, MAC_LEN);
   memcpy(arp.sender_mac, ifr.ifr_hwaddr.sa_data, MAC_LEN);
-  
+
   if (ioctl(fd, SIOCGIFADDR, &ifr) < 0)
     return 1;
-  
+
   /* Adding 2 bytes because of port shift */
-  memcpy(arp.sender_ip, ifr.ifr_addr.sa_data+PORT_SHIFT, IP_LEN);
+  memcpy(arp.sender_ip, ifr.ifr_addr.sa_data + PORT_SHIFT, IP_LEN);
 
   /* Print interface data */
   printf("Index of %s is %d\n", IF_NAME, if_index);
   printf("MAC Address: ");
-  for (i=0; i<MAC_LEN; i++)
-    printf("%X%c", eth.src[i], i==5?'\n':':');
+  for (i = 0; i < MAC_LEN; i++)
+    printf("%X%c", eth.src[i], i == 5 ? '\n' : ':');
 
   printf("IP Address:  ");
-  for (i=0; i<IP_LEN; i++)
-    printf("%d%c", arp.sender_ip[i], i==3?'\n':'.');
+  for (i = 0; i < IP_LEN; i++)
+    printf("%d%c", arp.sender_ip[i], i == 3 ? '\n' : '.');
 
   /* Create frame */
   memcpy(frame, &eth, sizeof(eth_header));
-  memcpy(frame+sizeof(eth_header), &arp, sizeof(arp_header));
-  
+  memcpy(frame + sizeof(eth_header), &arp, sizeof(arp_header));
+
   printf(PRINT_DELIM);
 
-
-  
   /* Print frame per byte */
   printf("Frame:       ");
-  for (i=0; (long unsigned int)i<sizeof(frame); i++)
-    printf("%02X%c",
-	   *(frame+i) & 0xFF,
-	   ((long unsigned int)i<sizeof(frame)-1)?' ':'\n');
+  for (i = 0; (long unsigned int)i < sizeof(frame); i++)
+    printf("%02X%c", *(frame + i) & 0xFF,
+           ((long unsigned int)i < sizeof(frame) - 1) ? ' ' : '\n');
 
   printf(PRINT_DELIM);
-  
+
   /* Create request */
-  sll.sll_family   = PF_PACKET;
-  sll.sll_ifindex  = if_index;
+  sll.sll_family = PF_PACKET;
+  sll.sll_ifindex = if_index;
   sll.sll_protocol = 0x0806;
 
   /* Send request */
-  if (sendto(fd, frame, sizeof(frame), 0, (struct sockaddr *)&sll, sizeof(sll)) == -1)
+  if (sendto(fd, frame, sizeof(frame), 0, (struct sockaddr *)&sll,
+             sizeof(sll)) == -1)
     return -1;
-  
+
   /* Receive answer */
   for (;;) {
     /* Get all answers */
     if ((ret = recv(fd, rbuf, sizeof(rbuf), 0)) == -1)
       return -1;
     /* Filter only ARP answers */
-    if (memcmp(rbuf+REQUEST_TYPE_SHIFT, &eth.type, 2) == 0) {
+    if (memcmp(rbuf + REQUEST_TYPE_SHIFT, &eth.type, 2) == 0) {
       /* Print Answer MAC address */
       printf("Target MAC:  ");
-      for (i=0; i<MAC_LEN; i++)
-	printf("%X%c", *(rbuf+PROTOCOL_TYPE_SHIFT+i), i<5?':':'\n');
+      for (i = 0; i < MAC_LEN; i++)
+        printf("%X%c", *(rbuf + PROTOCOL_TYPE_SHIFT + i), i < 5 ? ':' : '\n');
       break;
-      }
+    }
   }
   return 0;
 }
-
--- icmp/main.c
+++ icmp/main.c
@@ -2,24 +2,24 @@
 #include <net/if.h>
 #include <netdb.h>
 #include <netinet/in.h>
+#include <netpacket/packet.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/ioctl.h>
 #include <sys/socket.h>
-#include <netpacket/packet.h>
 
 #define MAC_LEN 6
 
-#define PRINT_DELIM \
+#define PRINT_DELIM                                                            \
   "=========================================================\n"
 
-typedef unsigned char  u8;
+typedef unsigned char u8;
 typedef unsigned short u16;
 
 typedef struct {
-  u8  dst[MAC_LEN];
-  u8  src[MAC_LEN];
+  u8 dst[MAC_LEN];
+  u8 src[MAC_LEN];
   u16 type;
 } eth_header;
 
@@ -46,94 +46,83 @@ typedef struct {
   u16 seq_number;
 } icmp_header;
 
-u16 calculate_checksum(ipv4_header *ipv4){
-
-  u16 *data = (u16*) ipv4;
+u16 calculate_checksum(ipv4_header *ipv4) {
+  u16 *data = (u16 *)ipv4;
 
   const size_t data_len = 28 / 2; /* double-bytes size */
   size_t i;
-  
+
   u16 res = 0;
 
-  for (i=0; i<data_len; i++)
+  for (i = 0; i < data_len; i++)
     if (i != 5) /* checksum location */
       res += htons(data[i]);
-  
+
   return res;
 }
 
-int main(){
+int main() {
 
-  u8 frame[] = {
-    0x78, 0x9a, 0x18, 0x33, 0x29, 0x6a,
-    0xe0, 0x2b, 0xe9, 0xab, 0xa2, 0x39,
-    0x08, 0x00, 0x45, 0x00, 0x00, 0x54,
-    0x02, 0x7e, 0x40, 0x00, 0x40, 0x01,
-    0xb2, 0xd7, 0xc0, 0xa8, 0x02, 0x02,
-    0xc0, 0xa8, 0x02, 0x01, 0x08, 0x00,
-    0x3c, 0xa5, 0xb9, 0xac, 0x00, 0x01,
-    0xe5, 0x6a, 0xe1, 0x67, 0x00, 0x00,
-    0x00, 0x00, 0x70, 0x07, 0x0c, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x10, 0x11,
-    0x12, 0x13, 0x14, 0x15, 0x16, 0x17,
-    0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d,
-    0x1e, 0x1f, 0x20, 0x21, 0x22, 0x23,
-    0x24, 0x25, 0x26, 0x27, 0x28, 0x29,
-    0x2a, 0x2b, 0x2c, 0x2d, 0x2e, 0x2f,
-    0x30, 0x31, 0x32, 0x33, 0x34, 0x35,
-    0x36, 0x37};
-    
-  
+  u8 frame[] = {0x78, 0x9a, 0x18, 0x33, 0x29, 0x6a, 0xe0, 0x2b, 0xe9, 0xab,
+                0xa2, 0x39, 0x08, 0x00, 0x45, 0x00, 0x00, 0x54, 0x02, 0x7e,
+                0x40, 0x00, 0x40, 0x01, 0xb2, 0xd7, 0xc0, 0xa8, 0x02, 0x02,
+                0xc0, 0xa8, 0x02, 0x01, 0x08, 0x00, 0x3c, 0xa5, 0xb9, 0xac,
+                0x00, 0x01, 0xe5, 0x6a, 0xe1, 0x67, 0x00, 0x00, 0x00, 0x00,
+                0x70, 0x07, 0x0c, 0x00, 0x00, 0x00, 0x00, 0x00, 0x10, 0x11,
+                0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b,
+                0x1c, 0x1d, 0x1e, 0x1f, 0x20, 0x21, 0x22, 0x23, 0x24, 0x25,
+                0x26, 0x27, 0x28, 0x29, 0x2a, 0x2b, 0x2c, 0x2d, 0x2e, 0x2f,
+                0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37};
+
   int fd;
 
   eth_header eth = {
-    .dst	= {0x78, 0x9a, 0x18, 0x33, 0x29, 0x6a},
-    .src	= {0xe0, 0x2b, 0xe9, 0xab, 0xa2, 0x39},
-    .type	= htons(0x0800),
+      .dst = {0x78, 0x9a, 0x18, 0x33, 0x29, 0x6a},
+      .src = {0xe0, 0x2b, 0xe9, 0xab, 0xa2, 0x39},
+      .type = htons(0x0800),
   };
 
   ipv4_header ipv4 = {
-    .version_ihl		= (0x45),
-    .type_of_service		= 0x00,
-    .total_length		= htons(0x0054),
-    .identivication		= htons(0x027e),
-    .flags_fragment_offset	= htons(0x4000),
-    .ttl			= (0x40),
-    .protocol			= (0x01),
-    .header_checksum		= htons(0xb2d7), /* TODO: create function to calculate checksum */
-    .src_address		= {192, 168, 2, 2},
-    .dst_address		= {192, 168, 2, 1},
+      .version_ihl = (0x45),
+      .type_of_service = 0x00,
+      .total_length = htons(0x0054),
+      .identivication = htons(0x027e),
+      .flags_fragment_offset = htons(0x4000),
+      .ttl = (0x40),
+      .protocol = (0x01),
+      .header_checksum =
+          htons(0xb2d7), /* TODO: create function to calculate checksum */
+      .src_address = {192, 168, 2, 2},
+      .dst_address = {192, 168, 2, 1},
   };
-  
+
   fd = socket(PF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
   if (fd == -1)
     return 1;
 
   struct sockaddr_ll sll = {0};
-  
+
   /* Create request */
-  sll.sll_family   = PF_PACKET;
-  sll.sll_ifindex  = 3;
+  sll.sll_family = PF_PACKET;
+  sll.sll_ifindex = 3;
   sll.sll_protocol = 0x0806;
 
   memcpy(frame, &eth, sizeof(eth_header));
 
-
-  for (size_t i=sizeof(eth_header);
-       i<sizeof(eth_header)+sizeof(ipv4_header);
-       i++)
+  for (size_t i = sizeof(eth_header);
+       i < sizeof(eth_header) + sizeof(ipv4_header); i++)
     printf("%02X ", frame[i]);
   printf("\n\n");
 
-  memcpy(frame+sizeof(eth_header), &ipv4, sizeof(ipv4_header));
+  memcpy(frame + sizeof(eth_header), &ipv4, sizeof(ipv4_header));
 
-  for (size_t i=sizeof(eth_header);
-       i<sizeof(eth_header)+sizeof(ipv4_header);
-       i++)
+  for (size_t i = sizeof(eth_header);
+       i < sizeof(eth_header) + sizeof(ipv4_header); i++)
     printf("%02X ", frame[i]);
   printf("\n");
-  
-  if (sendto(fd, frame, sizeof(frame), 0, (struct sockaddr *)&sll, sizeof(sll)) == -1)
+
+  if (sendto(fd, frame, sizeof(frame), 0, (struct sockaddr *)&sll,
+             sizeof(sll)) == -1)
     return -1;
 
   printf("%X\n", (calculate_checksum(&ipv4)));
